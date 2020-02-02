88416fdd79ceea9e84b0e076e5e69988f4cd3d24
animevn <haanhth21@gmail.com>
2020-02-02 10:08:41 +0700

    reset

--- app/src/main/java/com/haanhgs/swipebackfragment/FragmentHome.java
+++ app/src/main/java/com/haanhgs/swipebackfragment/FragmentHome.java
@@ -1,67 +1,8 @@
 package com.haanhgs.swipebackfragment;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentTransaction;
+import com.haanhgs.swipebackfragment.lib.SwipeBackFragment;
 
-public class FragmentHome extends BaseFragment {
+public class FragmentHome extends SwipeBackFragment {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("FragmentHome", "on Create");
-    }
 
-    private void openFragmentTest(){
-        if (getFragmentManager() != null){
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            FragmentTest1 fragment = (FragmentTest1)getFragmentManager().findFragmentByTag("test1");
-            if (fragment == null){
-                FragmentTest1 fragmentTest = new FragmentTest1();
-                ft.add(R.id.flHome, fragmentTest, "test1");
-                ft.hide(FragmentHome.this);
-                ft.addToBackStack("test1");
-                ft.commit();
-            }else {
-                ft.attach(fragment);
-            }
-        }
-    }
-
-    @Override
-    protected void handleTextView() {
-        super.handleTextView();
-        super.tvMain.setText(R.string.home);
-    }
-
-    @Override
-    protected void handleButton() {
-        super.handleButton();
-        super.bnMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragmentTest();
-            }
-        });
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d("FragmentHome", "on Resume");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.d("FragmentHome", "on DestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d("FragmentHome", "on Destroy");
-    }
 }
